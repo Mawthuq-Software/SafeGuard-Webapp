7fa22e7507f0fcf8ce2c2d3b0e789fb306e51e1d
hoodedice <hoodedice@gmail.com>
2022-12-03 19:00:23 -0800

    Add checkbox and TextLink components

---
+++ components/Checkbox.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="checkbox-container" @click="toggle">
+    <div :class="checkboxClass">
+      <img src="/tick.svg" v-show="checked" />
+    </div>
+    <div v-if="label" class="checkbox-label">
+      <label>{{label}}</label>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+  const props = defineProps({
+    initialChecked: Boolean,
+    label: String,
+  });
+
+  // checked state boolean initialize to initialChecked
+  const checked = ref(props.initialChecked ?? false);
+
+  // emit checked state on change
+  watch(checked, (newChecked) => {
+    console.log("change", newChecked);
+  });
+
+  function toggle() {
+    checked.value = !checked.value;
+  }
+
+  const checkboxClass = computed(() => ({
+    checked: checked.value,
+    checkbox: true
+  }));
+</script>
---
+++ components/TextLink.vue
@@ -0,0 +1,3 @@
+<template>
+  <span class="text-primary-600 w"><slot /></span>
+</template>
